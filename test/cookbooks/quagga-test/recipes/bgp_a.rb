52884d4c56b859cb421f37aa1e26e993450d3ee8
Ian Clark <ian@f85.net>
2015-10-22 00:23:16 -0700

    Fixing tests

--- test/cookbooks/quagga-test/recipes/bgp_a.rb
+++ test/cookbooks/quagga-test/recipes/bgp_a.rb
@@ -7,6 +7,6 @@ node.set[:quagga][:bgp]['64512'][:log_neighbor_changes] = true
 node.set[:quagga][:bgp]['64512'][:neighbors]['hosts'][:remote_as] = 64512
 node.set[:quagga][:bgp]['64512'][:neighbors]['hosts'][:default_originate] = true
 node.set[:quagga][:bgp]['64512'][:neighbors]['hosts'][:peer_group] = true
-node.set[:quagga][:bgp]['64512'][:neighbors]['hosts'][:peer_group_range] = '10.6.11.0/24'
+node.set[:quagga][:bgp]['64512'][:neighbors]['hosts'][:peer_group_range] = '10.0.0.0/8'
 
-include_recipe 'quagga::bgp'
+include_recipe 'quagga::bgpd'
--- test/cookbooks/quagga-test/recipes/default_a.rb
+++ test/cookbooks/quagga-test/recipes/default_a.rb
@@ -3,7 +3,7 @@
 # Recipe:: default_a
 #
 
-node.set[:quagga][:router_id] = '4.0.0.1'
+node.set[:quagga][:router_id] = '127.0.0.1'
 node.set[:quagga][:integrated_vtysh_config] = true
 
 include_recipe "quagga-test::ospf_a"
