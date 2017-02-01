cfa97da0bd81034d5e48e44163121c6ecf8713f5
nertwork <webmaster@nertwork.com>
2017-02-01 11:15:29 -0800

    changing more testing stuff

--- test/cookbooks/quagga-test/recipes/ospf6_a.rb
+++ test/cookbooks/quagga-test/recipes/ospf6_a.rb
@@ -5,7 +5,7 @@
 
 node.set[:quagga][:ospf6][:redistribute] = ["static"]
 node.set[:quagga][:ospf6][:router_id] = "127.0.0.1"
-node.set[:quagga][:ospf6][:areas]['0.0.0.1'][:interfaces] = {'spw1', 'swp2'}
-node.set[:quagga][:ospf6][:areas]['1.1.1.1'][:interfaces] = {'swp4', 'swp5'}
+node.set[:quagga][:ospf6][:areas]['0.0.0.1'][:interfaces] = ['spw1', 'swp2']
+node.set[:quagga][:ospf6][:areas]['1.1.1.1'][:interfaces] = ['swp4', 'swp5']
 
 include_recipe 'quagga::ospf6d'
