9434295f943203e816c0488dfc69b1fe3f515f12
nertwork <webmaster@nertwork.com>
2017-05-10 14:02:37 -0700

    Fixing test for bgp_c

--- test/cookbooks/quagga-test/recipes/bgp_c.rb
+++ test/cookbooks/quagga-test/recipes/bgp_c.rb
@@ -9,5 +9,6 @@ node.set['quagga']['bgp']['64512']['neighbors']['hosts']['peer_type'] = 'peer-gr
 node.set['quagga']['bgp']['64512']['neighbors']['hosts']['ipv6'] = true
 node.set['quagga']['bgp']['64512']['neighbors']['hosts']['default_originate_v6'] = true
 node.set['quagga']['bgp']['64512']['neighbors']['hosts']['peer_group_range'] = ['2600::/64', '1000:1::/64']
+node.set['quagga']['bgp']['64512']['address_family']['ipv6'] = {}
 
 include_recipe 'quagga::bgpd'
